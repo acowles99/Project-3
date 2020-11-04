1630dbb4368baa8ab557cbc6315aeeccd676c375
Alexis Cowles <alexis.cowles@patelhs.org>
2020-11-04 17:51:31 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+   int [] seatNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+   String [] elements = {"Fast", "Adventurous", "Daring", "Happy", "Intellegent", "Patient","Skillful", "Mighty", "Dedicated","Creative","Loyal", "Humble", "Wise", "Tall", "Elegant"};
+
+   for(int i = 0; i < elements.length; i++)
+   {
+     Scanner name = new Scanner (System.in);
+     System.out.println("Please enter your name");
+     String nameEntered
+     System.out.println(elements[i]);
+   }
+  }
+}
