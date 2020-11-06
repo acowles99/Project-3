2d805d446a1e4cdf8daca5122ba65a3d8c4ebc0e
Alexis Cowles <alexis.cowles@patelhs.org>
2020-11-06 16:03:21 +0000

    finished the code

--- Main.java
+++ Main.java
@@ -5,17 +5,23 @@ class Main
 
   public static void main(String[] args) {
    int [] seatNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-   String [] element = {"Fast", "Adventurous", "Daring", "Happy", "Intellegent", "Patient","Skillful", "Mighty", "Dedicated","Creative","Loyal", "Humble", "Wise", "Tall", "Elegant"};
+   String [] names = new String [15];
    //elements
 
    Scanner scan = new Scanner (System.in);
-   System.out.println("Please enter your name");
-   String name = scan.next();
+   
    for(int i = 0; i < 15; i++)
    {
-    System.out.println(element[i] + " " + name);
+    System.out.println("Please enter your name");
+     names [i] = scan.next();
    }
-   
+   //scan and for loop to collect students names
+   int index = 0; 
+   while (index < 15)
+   {
+     System.out.println(names[index]+ " sits in seat #" +seatNumbers[index]);
+     index++;
+   }
    
   }
 }
